9ec934977e95525fec3b6d5d993f787d00096acc
LuisMiguelDaSilvaPacheco <lm.luismiguel10@gmail.com>
2025-11-15 17:49:40 -0300

    Update tentativas.php

--- tentativas.php
+++ tentativas.php
@@ -4,18 +4,17 @@ $chute = 0;
 $sorteado = rand(1, 100);
 $tentativas = 0;
 
-while ($chute != 1){
+while ($chute != $sorteado) {
     $tentativas++;
     $chute = (int) readline("digite um número: ");
-    $sorteado = rand(1, 100);
 
-    if ($chute === $sorteado){
+    if ($chute === $sorteado) {
         print("\033[32mvocê venceu! \n");
-    }else{
-        if ($chute > $sorteado){
-            print("\033[31mvocê perdeu, tente chutar mais baixo na próxima \033[0m\n");
-        }else{
-            print("\033[31mvocê perdeu, tente chutar mais alto na próxima \033[0m\n");
+    } else {
+        if ($chute > $sorteado) {
+            print("\033[31mNão foi dessa vez... Tente chutar mais baixo na próxima! \033[0m\n");
+        } else {
+            print("\033[31mNão foi dessa vez... Tente chutar mais alto na próxima! \033[0m\n");
         }
     }
 }
